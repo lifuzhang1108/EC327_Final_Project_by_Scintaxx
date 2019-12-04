eba8da075d1f5e89c2c75a67b73b87ef4eec14a7
Alex Zhang <1309701864@qq.com>
2019-12-03 19:05:30 -0500

    add files

---
+++ app/src/main/java/com/webianks/scintaxxmessenger/adapters/ItemCLickListener.java
@@ -0,0 +1,7 @@
+package com.webianks.scintaxxmessenger.adapters;
+
+
+public interface ItemCLickListener {
+    void itemClicked(int color,String contact,long id,String read);
+    //void itemLongClicked(int position,String contact,long id);
+}
---
+++ app/src/main/java/com/webianks/scintaxxmessenger/receivers/DeliverReceiver.java
@@ -0,0 +1,29 @@
+package com.webianks.scintaxxmessenger.receivers;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.webianks.scintaxxmessenger.R;
+
+
+
+public class DeliverReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent arg1) {
+        switch (getResultCode()) {
+            case Activity.RESULT_OK:
+                Toast.makeText(context, R.string.sms_delivered,
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case Activity.RESULT_CANCELED:
+                Toast.makeText(context, R.string.sms_not_delivered,
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+    }
+}
